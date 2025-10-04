3d4cc256688f0efc574ba44f32cfd6951a542e59
Klauss F. Wuth A. <kfwuth@gmail.com>
2025-10-04 10:20:51 -0300

    Adds function to toggle video controls

--- lib/src/chewie_player.dart
+++ lib/src/chewie_player.dart
@@ -494,7 +494,7 @@ class ChewieController extends ChangeNotifier {
   final bool showControlsOnInitialize;
 
   /// Whether or not to show the controls at all
-  final bool showControls;
+  bool showControls;
 
   /// Controller to pass into the [InteractiveViewer] component.
   /// If it is required to control the transformation only via the controller,
@@ -657,6 +657,21 @@ class ChewieController extends ChangeNotifier {
 
   void toggleFullScreen() {
     _isFullScreen = !_isFullScreen;
+    notifyListeners();
+  }
+
+  void hideControls() {
+    showControls = false;
+    notifyListeners();
+  }
+
+  void displayControls() {
+    showControls = true;
+    notifyListeners();
+  }
+
+  void toggleControls() {
+    showControls = !showControls;
     notifyListeners();
   }
 
